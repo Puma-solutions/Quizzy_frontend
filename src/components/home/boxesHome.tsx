f87ef36d451102b23b51930a2c521c1ff647f946
Tomas Izuel <tomasizuel@gmail.com>
2023-04-12 17:44:04 -0300

    animation gome

---
+++ src/components/home/boxesHome.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../../../styles/home.css";
+
+const BoxesHome = () => {
+  return (
+    <div>
+      <div className="boxes">
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoxesHome;
